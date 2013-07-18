08df45d782945f28426df3ced222a26578c698e6
Amann Malik <amannmalik@gmail.com>
2013-07-18 17:39:48 -0500

    Initial Commit

---
+++ DL.h
@@ -0,0 +1,35 @@
+struct Concept;
+struct Role;
+
+typedef struct Results {
+	Concept** superConcepts;
+	int size;
+};
+
+Concept* Concept_ExistentialRoleRestriction_create(Role*, Concept*);
+Concept* Concept_Conjunction_create(Concept**, int);
+Concept* Concept_Atomic_create();
+void Concept_destroy(Concept*);
+
+Role* Role_create();
+void Role_destroy(Role*);
+
+void Axiom_ConceptInclusion_apply(Concept*, Concept*);
+void Axiom_ConceptEquivalence_apply(Concept*, Concept*);
+void Axiom_ConceptDisjointedness_apply(Concept*, Concept*);
+
+
+void Axiom_RoleInclusion_apply(Concept*, Concept*);
+void Axiom_RoleEquivalence_apply(Concept*, Concept*);
+
+	
+void Axiom_TransitiveRole_apply(Concept*, Concept*);
+void Axiom_ReflexiveRole_apply(Concept*, Concept*);
+
+void Axiom_DomainRestriction_apply(Concept*, Concept*);
+void Axiom_RangeRestriction_apply(Concept*, Concept*);
+
+void Reasoner_initialize(Concept*);
+void Reasoner_start();
+void Concept_Subsumers_query(Concept*, Results*);
+
